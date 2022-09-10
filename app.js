6967d8dc8335b75b937d66838c6dd20a59b107b4
TungLe <thanhtung12121212@gmail.com>
2022-09-10 17:43:39 +0700

    update

--- app.js
+++ app.js
@@ -91,15 +91,15 @@ app.use(staffRoutes);
 app.use(authRoutes)
 
 app.use(errorController.get404);
-// app.get('/500', errorController.get500)
+app.get('/500', errorController.get500)
 
-// app.use((error, req, res, next) => {
-//     res.status(500).render('500', {
-//         pageTitle: 'Server error',
-//         path: '/500',
-//         isAuthenticated: req.session.isLoggedIn
-//     });
-// })
+app.use((error, req, res, next) => {
+    res.status(500).render('500', {
+        pageTitle: 'Server error',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn
+    });
+})
 
 mongoose
     .connect(MONGODB_URI)
